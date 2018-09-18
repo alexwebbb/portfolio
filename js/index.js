0ff02e9e6ae301c077f37df359147d1d5a41600c
Alex Webb <alexwebbb@protonmail.com>
2018-09-17 21:26:04 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//   $(".pushpin").pushpin();
+  $(".pushpin-demo-nav").each(function() {
+    var $this = $(this);
+    var $target = $("#" + $(this).attr("data-target"));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+});
