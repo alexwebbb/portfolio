4de88605b61c3191f9d4ea01c6b50c6e6c256634
Alex Webb <alexwebbb@protonmail.com>
2018-09-20 05:30:55 -0500

    updating to koa

---
+++ app.js
@@ -0,0 +1,48 @@
+const Koa = require("koa"),
+  app = new Koa(),
+  views = require("koa-views"),
+  json = require("koa-json"),
+  onerror = require("koa-onerror"),
+  bodyparser = require("koa-bodyparser"),
+  logger = require("koa-logger"),
+  // routes
+  index = require("./routes/index"),
+  users = require("./routes/users");
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"]
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
+
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug"
+  })
+);
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
