d9e36213b6e2930c266c17e5e2e606c1b45157c3
Alex Webb <alexwebbb@protonmail.com>
2018-09-26 06:31:32 -0500

    added support for sass

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,6 @@
-const router = require("koa-router")();
+const router = require("koa-router")(),
+  { index } = require("../controllers/indexController");
 
-router.get("/", async (ctx, next) => {
-  await ctx.render("index", {
-    title: "Hello Koa 2!"
-  });
-});
-
-router.get("/string", async (ctx, next) => {
-  ctx.body = "koa2 string";
-});
-
-router.get("/json", async (ctx, next) => {
-  ctx.body = {
-    title: "koa2 json"
-  };
-});
+router.get("/", index);
 
 module.exports = router;
