70e9ad496cae694884c8f45e04de4282e5f65746
Alex Webb <alexwebbb@protonmail.com>
2018-10-02 04:41:44 -0500

    added gallery and images

--- controllers/indexController/projects.js
+++ controllers/indexController/projects.js
@@ -18,21 +18,30 @@ const {
 module.exports = [
   {
     name: "Earthworm",
-    images: ["", "", ""],
+    images: [
+      "https://res.cloudinary.com/execool/image/upload/v1538470637/portfolio/earthworm-full.jpg",
+      "https://res.cloudinary.com/execool/image/upload/v1538470636/portfolio/earthworm-detail.jpg"
+    ],
     href: "alexwebbb.github.io/thinkful-async-web-apps/api-hack/",
     description: `Earthworm is a visualization of Google's Elevation API utilizing D3. A square is shown on the map. You can move the square around and change it's shape. The view will update, in real time.`,
     techUsed: [D3JS, JQUERY, BOOTSTRAP]
   },
   {
     name: "ItemStore",
-    images: ["", "", ""],
+    images: [
+      "https://res.cloudinary.com/execool/image/upload/v1538470637/portfolio/itemstore-full.jpg",
+      "https://res.cloudinary.com/execool/image/upload/v1538470634/portfolio/itemstore-detail.jpg"
+    ],
     href: "itemstore.info/store/",
     description: `Item Store is a mini web store, with basic session tracking. After you create an account, your views are tracked and saved as sessions, which can be viewed later. Complete with shopping cart, payment processing and order history.`,
     techUsed: [NODEJS, EXPRESSJS, MONGODB, BOOTSTRAP, PASSPORT, STRIPE, CHARTJS]
   },
   {
     name: "AstroApp",
-    images: ["", "", ""],
+    images: [
+      "https://res.cloudinary.com/execool/image/upload/v1538470636/portfolio/astroapp-full.jpg",
+      "https://res.cloudinary.com/execool/image/upload/v1538470634/portfolio/astroapp-detail.jpg"
+    ],
     href: "astrology-app.herokuapp.com",
     description: `Astro App lets you see how you would rate romantically with someone else if you were to break astrological signs down into numbers. Purchase credits to add profiles, and see graphs of the comparison data. utilizing sample info from FamousBirthdays.com. No real money involved, transactions are for simulation purposes only.`,
     techUsed: [
