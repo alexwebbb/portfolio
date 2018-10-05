db9d235199b44f3e754e792ac7ccce7a0717ca0b
Alex Webb <alexwebbb@protonmail.com>
2018-10-05 06:34:53 -0500

    webpack bundling implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './style.scss';
+import 'bootstrap';
+import 'ekko-lightbox';
+
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
