1d0e46d35cb064fd852655c249b85fbac83e68d0
Alex Webb <alexwebbb@protonmail.com>
2018-10-22 13:10:11 -0500

    tab version implemented

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/scrollspy';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/util';
 import 'ekko-lightbox';
 import * as cloudinary from 'cloudinary-core/cloudinary-core-shrinkwrap';
