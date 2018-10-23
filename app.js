58ccc759ad62cef92867e8fc48e0c114053efd79
Alex Webb <alexwebbb@protonmail.com>
2018-10-22 22:45:45 -0500

    trying to get favicon working

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
   app.use(json());
   app.use(logger());
   app.use(require("koa-static")(__dirname + "/public"));
-  app.use(favicon(__dirname + '/public/favicon.ico'));
+  // app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(
   views(__dirname + "/views", {
